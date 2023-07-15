b105dabfdad9fb032134251686113c03be0dfb6f
Michael Lutz <michaeljeffreylutz@gmail.com>
2023-07-15 14:17:16 -0700

    Added translate function w/ efficient generation + log prob calculation

--- translator.py
+++ translator.py
@@ -1,4 +1,6 @@
 import openai
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 class Source:
     def __init__(self) -> None:
@@ -35,12 +37,47 @@ class ConvoBuffer:
 #     for res in openai.Completion.create(model="text-davinci-003", prompt=prompt,  max_tokens=7, temperature=0, stream=True, logprobs=5):
 #         yield res["choices"][0]["text"], res["choices"][0]["logprobs"]["token_logprobs"]
 
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-zh-en")
+
 async def process_buffer(text):
     res = await openai.Completion.acreate(model="text-davinci-003", prompt=text,  max_tokens=32, temperature=0, stream=True, logprobs=5)
     return text, res["choices"][0]["text"], res["choices"][0]["logprobs"]["token_logprobs"]
 
-def translate(text, translated_conversation_history, untranslated_conversation_history):
-    return text, None, None
+def translate(left_to_translate, translated_conversation_history, untranslated_conversation_history):
+    input_text = untranslated_conversation_history + left_to_translate
+    encoded_input = tokenizer.encode(input_text, return_tensors="pt")
+
+    # Tokenize and process forced start-of-generation tokens
+    generated_ids = tokenizer.encode(translated_conversation_history, return_tensors="pt")[0][:-1].unsqueeze(0)
+    generated_ids = torch.cat((torch.tensor(tokenizer.pad_token_id).unsqueeze(0), generated_ids[0]), dim=0).unsqueeze(0)
+
+    # Initialize logits and tokens lists
+    log_probabilities = []
+    tokens_list = torch.empty(0)
+
+    # Keep generating until EOS token
+    while True:        
+        outputs = model(input_ids=encoded_input, decoder_input_ids=generated_ids)
+        
+        # Get logits and token of the last position
+        logits, token = outputs.logits[:, -1, :], torch.argmax(outputs.logits[:, -1, :], dim=1)
+        
+        # Add predicted token to tokens_list and update generated_ids
+        tokens_list = torch.cat((tokens_list, token), dim=-1)
+        generated_ids = torch.cat((generated_ids, token.unsqueeze(0)), dim=-1)
+
+        # Appends log probability of most-likely token
+        log_probabilities.append(torch.nn.functional.log_softmax(logits, dim=1)[0].max().item())
+
+        # Breaks if EOS token is presented
+        if token == tokenizer.eos_token_id:
+            break
+
+    # Getting the translated text
+    translated_text = tokenizer.decode(tokens_list, skip_special_tokens=True)
+    return translated_text, log_probabilities
+
 
 class Translator:
     def __init__(self) -> None:
@@ -65,7 +102,7 @@ class Translator:
     
     def add_chunk(self, text):
         self.untranslated_text += text
-        text, translated_text, logprobs = translate(text)
+        text, translated_text, logprobs = translate(text) # FIX THIS
         translation = self.process_translation(text, translated_text, logprobs)
         if translation is not None:
             self.send_to_tts(translation)
