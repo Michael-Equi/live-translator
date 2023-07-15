78a63241743091d7f06d751d9eda0c8cd9d667a3
Michael Equi <michaelequi@berkeley.edu>
2023-07-15 12:32:39 -0700

    Updated process buffer interface

--- translator.py
+++ translator.py
@@ -25,9 +25,11 @@ class ConvoBuffer:
         self.convo_buffer += text
 
     def pop(self, text):
-        assert text in self.convo_buffer, ""
+        assert text in self.convo_buffer, "text must be in conv buffer"
         assert text[0:len(text)] == self.convo_buffer[0:len(text)], "text must come from the front of the convo buffer"
-        self.convo_buffer = self.convo_buffer[len(text):]
+        poped_buffer = self.convo_buffer[len(text):]
+        self.convo_buffer[len(text):] = ""
+        return poped_buffer
 
     def get_text(self):
         return self.convo_buffer
@@ -42,14 +44,27 @@ class ConvoBuffer:
 
 async def process_buffer(text):
     res = await openai.Completion.acreate(model="text-davinci-003", prompt=text,  max_tokens=32, temperature=0, stream=True, logprobs=5)
-    return res["choices"][0]["text"], res["choices"][0]["logprobs"]["token_logprobs"]
+    return text, res["choices"][0]["text"], res["choices"][0]["logprobs"]["token_logprobs"]
 
 class Translator:
     def __init__(self) -> None:
+        self.conversation_history = ""
+        self.translated_text = ""
         self.convbuff = ConvoBuffer()
-        self.output_buffer = ConvoBuffer()
 
-    def process_translation(self, )
+    def process_translation(self, text, translation, logprobs, threshold = 0.5):
+        # Check if the text has already been added to the conversation history
+        if len(self.translated_text) >= len(text) and text == self.translated_text[-len(text):]:
+            # Text has already been translated
+            return 
+        elif 
+            # Sum the logprobs and divide by length
+            if sum(logprobs) / len(logprobs) > threshold:
+                self.conversation_history += self.convbuff.pop(translation)
+                self.send_to_tts(translation)
+
+    def send_to_tts(self, text):
+        print("Send to tts")
 
     def add_chunk(self, text):
         # Receive chunk from TTS stream
