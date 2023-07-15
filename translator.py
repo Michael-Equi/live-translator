2442c19b3c8d46966913b7210bb7238768961c4a
Michael Equi <michaelequi@berkeley.edu>
2023-07-15 11:32:19 -0700

    framework

---
+++ translator.py
@@ -0,0 +1,19 @@
+import openai
+
+class Source:
+    def __init__(self) -> None:
+        pass
+    pass
+
+class Sink:
+
+    def __init__(self, source):
+        pass
+
+class Translator:
+    def __init__(self) -> None:
+        self.convo_buffer = []
+
+
+def main():
+    pass
