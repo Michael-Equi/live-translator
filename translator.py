8355e18defa08fc8a20d0c7f7aeba4888eb03475
Michael Equi <michaelequi@berkeley.edu>
2023-07-15 12:11:41 -0700

    Added some of the translation code

--- translator.py
+++ translator.py
@@ -1,4 +1,11 @@
 import openai
+import dotenv
+import os
+import asyncio
+
+dotenv.load_dotenv()
+openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.organization = os.environ["OPENAI_ORG"]
 
 class Source:
     def __init__(self) -> None:
@@ -10,9 +17,48 @@ class Sink:
     def __init__(self, source):
         pass
 
+class ConvoBuffer:
+    def __init__(self) -> None:
+        self.convo_buffer = ""
+    
+    def add(self, text):
+        self.convo_buffer += text
+
+    def pop(self, text):
+        assert text in self.convo_buffer, ""
+        assert text[0:len(text)] == self.convo_buffer[0:len(text)], "text must come from the front of the convo buffer"
+        self.convo_buffer = self.convo_buffer[len(text):]
+
+    def get_text(self):
+        return self.convo_buffer
+
+# print(x["choices"][0]["logprobs"]["token_logprobs"])
+# print(x["choices"][0]["text"])
+
+# def callgpt(prompt):
+#     # Stream from openai
+#     for res in openai.Completion.create(model="text-davinci-003", prompt=prompt,  max_tokens=7, temperature=0, stream=True, logprobs=5):
+#         yield res["choices"][0]["text"], res["choices"][0]["logprobs"]["token_logprobs"]
+
+async def process_buffer(text):
+    res = await openai.Completion.acreate(model="text-davinci-003", prompt=text,  max_tokens=32, temperature=0, stream=True, logprobs=5)
+    return res["choices"][0]["text"], res["choices"][0]["logprobs"]["token_logprobs"]
+
 class Translator:
     def __init__(self) -> None:
-        self.convo_buffer = []
+        self.convbuff = ConvoBuffer()
+        self.output_buffer = ConvoBuffer()
+
+    def process_translation(self, )
+
+    def add_chunk(self, text):
+        # Receive chunk from TTS stream
+        self.convbuff.add(text["choices"][0])
+        # Send to translation process
+        task = asyncio.create_task(process_buffer(self.convbuff.get_text()))
+        # Add done callback
+        task.add_done_callback(self.process_translation)
+
 
 
 def main():
